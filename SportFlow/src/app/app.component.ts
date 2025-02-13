d82f5b51b3eddf9fba7efacb7e72e6d3749627a1
slavomir8505 <slavomirkana@gmail.com>
2025-02-13 08:16:02 +0100

    nove

--- SportFlow/src/app/app.component.ts
+++ SportFlow/src/app/app.component.ts
@@ -1,21 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent],
-  template: `
-    <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="" alt="logo" aria-hidden="true" />
-      </header>
-      <section class="content">
-        <app-home></app-home>
-      </section>
-    </main>
-  `,
-  styleUrls: ['./app.component.css'],
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'SportFlow';
