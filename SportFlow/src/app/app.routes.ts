ccf040a23a3ecdaa32d62cb723c5e22fed4ab76d
JurajSupolik <jurajsupolik@gmail.com>
2025-02-27 09:04:37 +0100

    1

--- SportFlow/src/app/app.routes.ts
+++ SportFlow/src/app/app.routes.ts
@@ -2,8 +2,12 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LaligaComponent } from './laliga/laliga.component';
+import { SpravyComponent } from './spravy/spravy.component';
+import { DnesComponent } from './dnes/dnes.component';
 
 export const routes: Routes = [
     {path: '',component: HomeComponent},
     {path:'laliga-component', component: LaligaComponent},
+    {path:'spravy-component', component: SpravyComponent },
+    {path:'dnes-component', component: DnesComponent },
 ];
