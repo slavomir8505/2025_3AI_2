56ff316f747d656920903b131436b04a2ffaf0d3
JurajSupolik <jurajsupolik@gmail.com>
2025-03-25 20:47:57 +0100

    routing pokus

--- SportFlow/src/app/app.routes.ts
+++ SportFlow/src/app/app.routes.ts
@@ -4,10 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { LaligaComponent } from './laliga/laliga.component';
 import { SpravyComponent } from './spravy/spravy.component';
 import { DnesComponent } from './dnes/dnes.component';
+import { AccountComponent } from './account/account.component';
+
+
 
 export const routes: Routes = [
     {path: '',component: HomeComponent},
     {path:'laliga-component', component: LaligaComponent},
     {path:'spravy-component', component: SpravyComponent },
     {path:'dnes-component', component: DnesComponent },
-];
+     {path:'account-component', component: AccountComponent },
+     
+      ]
