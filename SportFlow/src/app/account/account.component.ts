224bf0c4ed8b232a34634f776b93d4dc003fe6c1
slavomir8505 <slavomirkana@gmail.com>
2025-04-02 22:04:10 +0200

    button+main-content

--- SportFlow/src/app/account/account.component.ts
+++ SportFlow/src/app/account/account.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterModule, RouterOutlet } from '@angular/router';
+
+
 @Component({
   selector: 'app-account',
-  imports: [RouterModule,],
+  imports: [RouterModule,MatButtonModule,],
   templateUrl: './account.component.html',
   styleUrl: './account.component.css'
 })
