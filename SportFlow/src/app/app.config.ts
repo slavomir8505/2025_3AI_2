a07f51d3e2eb242cf391fe56564f3caa5732556b
Juraj Supolik <jurajsupolik@gmail.com>
2025-04-23 10:02:17 +0200

    firebase

--- SportFlow/src/app/app.config.ts
+++ SportFlow/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
@@ -8,3 +9,16 @@ import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),]
 };
+const firebaseConfig = {
+  apiKey: "AIzaSyC2YcuOqn7Mo3mbp00VygeLjyezlO2XVqc",
+  authDomain: "sportflow-d83ea.firebaseapp.com",
+  projectId: "sportflow-d83ea",
+  storageBucket: "sportflow-d83ea.firebasestorage.app",
+  messagingSenderId: "499865264964",
+  appId: "1:499865264964:web:d222c399eb0393fe73ed11",
+  measurementId: "G-3YM33E2CVT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
