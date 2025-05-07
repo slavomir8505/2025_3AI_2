5289f8f52d132348e26f9aee2ada49900d02b327
Juraj Supolik <jurajsupolik@gmail.com>
2025-05-07 09:44:55 +0200

    firebase done

--- SportFlow/src/app/app.config.ts
+++ SportFlow/src/app/app.config.ts
@@ -3,10 +3,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
 
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),]
-};
 const firebaseConfig = {
   apiKey: "AIzaSyC2YcuOqn7Mo3mbp00VygeLjyezlO2XVqc",
   authDomain: "sportflow-d83ea.firebaseapp.com",
@@ -16,4 +16,12 @@ const firebaseConfig = {
   appId: "1:499865264964:web:d222c399eb0393fe73ed11",
   measurementId: "G-3YM33E2CVT"
 };
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),importProvidersFrom(
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(firebaseConfig)
+  )]
+};
+
 
