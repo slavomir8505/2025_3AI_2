346a538d05439a3d1930fcb86f54842bf168bd95
slavomir8505 <slavomirkana@gmail.com>
2025-06-04 08:35:25 +0200

    jahoda

---
+++ SportFlow/src/app/services/spravy.service.ts
@@ -0,0 +1,22 @@
+import { Observable,map } from "rxjs";
+import { AngularFirestore } from "@angular/fire/compat/firestore";
+import { Injectable } from "@angular/core";
+import { Spravy } from "../interfaces/spravy.interface";
+
+
+@Injectable({
+    providedIn: 'root'
+  })
+  export class SpravyService {
+  
+  
+    constructor(private store: AngularFirestore) { }
+  
+    getListOfSpravy(): Observable<Spravy[]>{
+      return this.store.collection<Spravy>('Spravy').get().pipe(
+        map(snapshot =>
+          snapshot.docs.map(doc => ({ ...(doc.data()) as Spravy}))
+        )
+      )
+    }
+  }
