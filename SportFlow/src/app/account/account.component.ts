a83936ae31032cc345846d1813d5e9846ebb636a
slavomir8505 <slavomirkana@gmail.com>
2025-09-16 11:57:19 +0200

    147

--- SportFlow/src/app/account/account.component.ts
+++ SportFlow/src/app/account/account.component.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { MatButton } from '@angular/material/button';
 
 
 @Component({
+  standalone: true,
   selector: 'app-account',
-  imports: [RouterModule,MatButtonModule, MatToolbarModule,MatIconModule,],
-  templateUrl: './account.component.html',
-  styleUrl: './account.component.css'
+  imports: [CommonModule, FormsModule,],
+  template: `
+    <h2>Prihlásenie</h2>
+    <form (ngSubmit)="login()">
+      <input [(ngModel)]="email" name="email" placeholder="Email" required />
+      <input [(ngModel)]="password" name="password" placeholder="Heslo" type="password" required />
+      <button type="submit">Prihlásiť sa</button>
+    </form>
+  `
 })
 export class AccountComponent {
+  email = '';
+  password = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService.login(this.email, this.password)
+      .then(() => this.router.navigateByUrl('/settings'))
+      .catch(err => console.error('Chyba pri prihlasovaní:', err));
+  }
 }
